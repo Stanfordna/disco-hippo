809fa97452eaf0565aad3aa43ad28ae03202a921
Nicholas Stanford <stanfordnas@gmail.com>
2024-10-16 20:24:50 -0600

    2024/10/16
    Do mostly nothing. Project structure progress.

---
+++ src/yxorp/__init__.py
@@ -0,0 +1,11 @@
+"""
+Fantastic reverse proxy package
+"""
+import os
+
+def main():
+    """ Probably will delete later in favor of a run() function for the reverse proxy """
+    print("hello")
+
+if __name__ == "__main__":
+    main()
